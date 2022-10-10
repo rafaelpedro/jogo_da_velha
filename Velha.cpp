ea97e4bef311fdd3e0b5528e7a2071739fdefbf6
rafael <rafael.apedro95@gmail.com>
2022-10-09 23:50:09 -0300

    Correção impressão de jogo

--- Velha.cpp
+++ Velha.cpp
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void imprimejogo(char jogo[3][3]){
+	int l, c;
+	
+	printf("\n\n\t 0   1   2\n\n");
+			for(l = 0; l < 3; l++){
+				for(c = 0; c < 3; c++){
+					if(c == 0)
+						printf("\t");
+					printf(" %c ", jogo[l][c]);
+					if(c < 2)
+						printf("|");
+					if(c == 2)
+						printf("   %d", l);
+				}
+				if(l < 2)
+					printf("\n\t------------");
+					printf("\n");
+			}
+}
 
-
-
+int jogarnovamente(){
+	int opcao;
+	printf("\nDigite 1 para jogar novamente: \n");
+			scanf("%d", &opcao);
+return opcao; 
+}
 
 int main(){
 	
@@ -23,21 +46,9 @@ int main(){
 		}
 			do{
 			// imprimir jogo
-			printf("\n\n\t 0   1   2\n\n");
-			for(l = 0; l < 3; l++){
-				for(c = 0; c < 3; c++){
-					if(c == 0)
-						printf("\t");
-					printf(" %c ", jogo[l][c]);
-					if(c < 2)
-						printf("|");
-					if(c == 2)
-						printf("   %d", l);
-				}
-				if(l < 2)
-					printf("\n\t------------");
-					printf("\n");
-			}
+			
+			imprimejogo(jogo);
+			
 			// ler coordenadas
 			do{
 				printf("\nJOGADOR %d: Digite a linha e a coluna que deseja jogar: ", jogador);
@@ -60,46 +71,62 @@ int main(){
 			if(jogo[0][0] == '0' && jogo[0][1] == '0' && jogo[0][2] == '0' ||
 			   jogo[1][0] == '0' && jogo[1][1] == '0' && jogo[1][2] == '0' ||
 			   jogo[2][0] == '0' && jogo[2][1] == '0' && jogo[2][2] == '0'){
+			   	imprimejogo(jogo);
 			    printf("\nParabens! O Jogador 1 venceu por linha!\n");	
-			  	ganhou = 1; 
+			  	ganhou = 1;
+				opcao = jogarnovamente(); 
 			   }
 			if(jogo[0][0] == 'X' && jogo[0][1] == 'X' && jogo[0][2] == 'X' ||
 			   jogo[1][0] == 'X' && jogo[1][1] == 'X' && jogo[1][2] == 'X' ||
 			   jogo[2][0] == 'X' && jogo[2][1] == 'X' && jogo[2][2] == 'X'){
+			   	imprimejogo(jogo);
 			    printf("\nParabens! O Jogador 2 venceu por linha!\n");
 				ganhou = 1;	
+				opcao = jogarnovamente();
 			   }
 			
 			// alguem ganhou por coluna?
 			if(jogo[0][0] == '0' && jogo[1][0] == '0' && jogo[2][0] == '0' ||
 			   jogo[0][1] == '0' && jogo[1][1] == '0' && jogo[2][1] == '0' ||
 			   jogo[0][2] == '0' && jogo[1][2] == '0' && jogo[2][2] == '0'){
+			   	imprimejogo(jogo);
 			    printf("\nParabens! O Jogador 1 venceu por coluna!\n");
 				ganhou = 1;	
+				opcao = jogarnovamente();
 			   }
 			if(jogo[0][0] == 'X' && jogo[1][0] == 'X' && jogo[2][0] == 'X' ||
 			   jogo[0][1] == 'X' && jogo[1][1] == 'X' && jogo[2][1] == 'X' ||
 			   jogo[0][2] == 'X' && jogo[1][2] == 'X' && jogo[2][2] == 'X'){
+				imprimejogo(jogo);	
 			    printf("\nParabens! O Jogador 2 venceu por coluna!\n");
-				ganhou = 1;	
+				ganhou = 1;
+				opcao = jogarnovamente();
 			   }
 			// alguem ganhou por diagonal direita?
 			if(jogo[0][0] == '0' && jogo[1][1] == '0' && jogo[2][2] == '0'){
+				imprimejogo(jogo);
 			    printf("\nParabens! O Jogador 1 venceu pela diagonal!\n");
-				ganhou = 1;	
+				ganhou = 1;
+				opcao = jogarnovamente();
 			   }
 			if(jogo[0][0] == 'X' && jogo[1][1] == 'X' && jogo[2][2] == 'X'){
+				imprimejogo(jogo);
 			    printf("\nParabens! O Jogador 2 venceu pela diagonal!\n");
-				ganhou = 1;	
+				ganhou = 1;
+				opcao = jogarnovamente();	
 			   }	
 			// alguem ganhou por diagonal esquerda?
 			if(jogo[0][2] == '0' && jogo[1][1] == '0' && jogo[2][0] == '0'){
+				imprimejogo(jogo);
 			    printf("\nParabens! O Jogador 1 venceu pela diagonal!\n");
-				ganhou = 1;	
+				ganhou = 1;
+				opcao = jogarnovamente();	
 			   }
 			if(jogo[0][2] == 'X' && jogo[1][1] == 'X' && jogo[2][0] == 'X'){
+				imprimejogo(jogo);	
 			    printf("\nParabens! O Jogador 2 venceu pela diagonal!\n");
-				ganhou = 1;	
+				ganhou = 1;
+				opcao = jogarnovamente();
 			   }
 			   
 			   
@@ -109,10 +136,10 @@ int main(){
 			
 			if(ganhou == 0){
 		
+				imprimejogo(jogo);
 				printf("\nDeu velha\n");
 				
-			printf("\nDigite 1 para jogar novamente: \n");
-			scanf("%d", &opcao);
+		opcao = jogarnovamente();
 		}
 	}while(opcao == 1);
 	return 0;
